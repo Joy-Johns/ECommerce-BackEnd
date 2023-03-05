e8e687442eecfd90eee3b41d70ff6c6d97efff2c
Joy Johns <JRJ20716@gmail.com>
2023-03-05 17:57:40 -0500

    Executed association methods on sequelize models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {foreignKey: "category_id"});
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey: "category_id"});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {foreignKey: "product_id", through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {foreignKey: "tag_id", through: ProductTag})
 
 module.exports = {
   Product,
